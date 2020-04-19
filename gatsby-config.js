3e7d42bb0a2c2f1d0d49b4a238287c171abec3c3
Aaron Powell <me@aaron-powell.com>
2020-04-20 09:49:47 +1000

    Adding tailwind css

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,7 @@ module.exports = {
     },
     "gatsby-plugin-typescript-checker",
     "gatsby-plugin-eslint",
+    "gatsby-plugin-styled-components",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
