e9f72b64e60ac93d654896eab529826e896a511b
Aaron Powell <me@aaron-powell.com>
2020-06-25 17:02:28 +1000

    tweaking for ssr with localStorage

--- src/data/session.ts
+++ src/data/session.ts
@@ -2,6 +2,10 @@ import { v4 as uuid } from "uuid"
 const SESSION_KEY = "ONLINE_ORDER_SESSION"
 
 const getSession = () => {
+  if (typeof localStorage === "undefined") {
+    return uuid()
+  }
+
   let sessionId = localStorage.getItem(SESSION_KEY)
   if (!sessionId) {
     sessionId = uuid()
@@ -12,6 +16,9 @@ const getSession = () => {
 }
 
 const destroySession = () => {
+  if (typeof localStorage === "undefined") {
+    return
+  }
   localStorage.removeItem(SESSION_KEY)
 }
 
