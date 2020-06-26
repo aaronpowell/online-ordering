f8294e0c34d5672d50dae59f1463484b33900eb5
Aaron Powell <me@aaron-powell.com>
2020-06-26 16:59:20 +1000

    changing how apollo client is created to support SSR

--- src/data/apollo.ts
+++ src/data/apollo.ts
@@ -1,7 +1,14 @@
-import ApolloClient from "apollo-boost"
+import { ApolloClient } from "apollo-client"
+import { createHttpLink } from "apollo-link-http"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import "isomorphic-fetch"
 
-const client = new ApolloClient<object>({
-  uri: `${process.env.GATSBY_BACKEND_URI || ""}/api/graphql`,
+const client = new ApolloClient({
+  ssrMode: true,
+  link: createHttpLink({
+    uri: `${process.env.GATSBY_BACKEND_URI || ""}/api/graphql`,
+  }),
+  cache: new InMemoryCache(),
 })
 
 export { client }
